c71a4d4d1f089e2148a6373583e2d82772d5a330
Aaron Olson <aaronmichael.olson@gmail.com>
2024-05-26 00:56:26 +0000

    Update project

--- index.js
+++ index.js
@@ -3,6 +3,22 @@ const express = require('express');
 const cors = require('cors');
 const app = express();
 
+const mongoose = require('mongoose');
+const { Schema, model } = mongoose;
+const urlShortenerScema = new Schema(
+  {
+    number: Number,
+    baseThirtySix: String,
+    url: String,
+    createdAt: Date,
+    submissions: Number
+  }
+)
+
+const Url = model('Url',urlShortenerScema);
+
+mongoose.connect(process.env.MONGO_URL);
+
 // Basic Configuration
 const port = process.env.PORT || 3000;
 
@@ -14,11 +30,31 @@ app.get('/', function(req, res) {
   res.sendFile(process.cwd() + '/views/index.html');
 });
 
+app.use(express.urlencoded({
+  limit: '10mb',
+  extended: true
+}));
+
 // Your first API endpoint
 app.get('/api/hello', function(req, res) {
   res.json({ greeting: 'hello API' });
 });
 
+app.post('/api/shorturl', function(req, res) {
+  console.log(req.body);  
+});
+
 app.listen(port, function() {
   console.log(`Listening on port ${port}`);
 });
+
+// Mongo DB Functions
+
+const addUrl = (done) => {
+  let newUrl = new Url({number: "Jane Fonda", age: 84, favoriteFoods: ["eggs", "fish", "fresh fruit"]});
+
+  newUrl.save(function(err, data) {
+    if (err) return console.error(err);
+    done(null, data)
+  });
+};
