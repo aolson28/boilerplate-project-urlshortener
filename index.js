e7281d4731933b7875aca6db4cfad209ecc07cad
Aaron Olson <aaronmichael.olson@gmail.com>
2024-05-27 19:51:08 +0000

    updated

--- index.js
+++ index.js
@@ -5,6 +5,7 @@ const app = express();
 const dns = require('dns');
 
 const mongoose = require('mongoose');
+const { url } = require('inspector');
 const { Schema, model } = mongoose;
 const urlShortenerScema = new Schema(
   {
@@ -41,54 +42,50 @@ app.get('/api/hello', function(req, res) {
   res.json({ greeting: 'hello API' });
 });
 
-app.get('/api/shorturl/:shortUrl', function(req, res) {
-  let shortUrlString = req.params.shortUrl;
-  let urlDbIndex = Url.find({baseThirtySix: shortUrlString}).then((data) => {
+app.get('/api/shorturl/:shortUrlString', (req, res) => {
+  let urlString = req.params.shortUrlString;
+  Url.find({baseThirtySix: urlString}).then((data) => {
     if (data.length > 0) {
       res.redirect(data[0].url);
     }
   });
-});
-
+})
 
 app.post('/api/shorturl', function(req, res) {
-  dns.lookup(req.body.url,(err, addr, fam) => {
-    if(err) {
-      res.json({ error: 'invalid url' });
-    } else {
-      let urlString = req.body.url;
-      let urlDbIndex = Url.find({url: urlString}).then((data) => {
-        if (data.length > 0) {
-          // console.log(data, 'exists');
-          res.json({original_url: data[0].url, short_url: data[0].baseThirtySix});
-        } else {
-          let numberInt;
-          Url.find({}).sort({ number: -1 }).limit(1).then(function(data) {
-            // console.log(data[0].number);
-            numberInt = parseInt(data[0].number) + 1;
-            let numberBaseThirtySix = numberInt.toString(36);
-            let createdAtDate = new Date();
-            let submissionsInt = 1;
-            // console.log('numberInt before save: ', numberInt)
-            let newUrl = new Url({number: numberInt, baseThirtySix: numberBaseThirtySix, url: urlString, createdAt: createdAtDate, submissions: submissionsInt});
-            newUrl.save().then(function(data) {
-              // console.log(data,'- data');
-              res.json({original_url: urlString, short_url: numberBaseThirtySix});
-            }).catch((err) => {
-              if (err) {
-                console.error(err);
-              }
-            });
-            let urlDbIndexTwo = Url.find({url: urlString}).then((data) => console.log(data, 'added'));
+  console.log(URL.canParse(req.body.url));
+  if (URL.canParse(req.body.url)) {
+    let urlString = req.body.url;
+    let urlDbIndex = Url.find({url: urlString}).then((data) => {
+      if (data.length > 0) {
+        // console.log(data, 'exists');
+        res.json({original_url: data[0].url, short_url: data[0].baseThirtySix});
+      } else {
+        let numberInt;
+        Url.find({}).sort({ number: -1 }).limit(1).then(function(data) {
+          // console.log(data[0].number);
+          numberInt = parseInt(data[0].number) + 1;
+          let numberBaseThirtySix = numberInt.toString(36);
+          let createdAtDate = new Date();
+          let submissionsInt = 1;
+          let newUrl = new Url({number: numberInt, baseThirtySix: numberBaseThirtySix, url: urlString, createdAt: createdAtDate, submissions: submissionsInt});
+          newUrl.save().then(function(data) {
+            res.json({original_url: urlString, short_url: numberBaseThirtySix});
           }).catch((err) => {
             if (err) {
               console.error(err);
             }
           });
-        }
-      });
-    }
-  })
+          let urlDbIndexTwo = Url.find({url: urlString}).then((data) => console.log(data, 'added'));
+        }).catch((err) => {
+          if (err) {
+            console.error(err);
+          }
+        });
+      }
+    });
+  } else {
+    res.json({ error: 'invalid url' });
+  }
 });
 
 app.listen(port, function() {
