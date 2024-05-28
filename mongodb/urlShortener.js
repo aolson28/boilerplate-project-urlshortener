e647e6a7456bf1d464785485c556b76926228b24
Aaron Olson <aaronmichael.olson@gmail.com>
2024-05-28 01:18:51 +0000

    Clean Up project. Add connect to database module

--- mongodb/urlShortener.js
+++ mongodb/urlShortener.js
@@ -1,58 +1,30 @@
-require('dotenv').config();
-const express = require('express');
-const cors = require('cors');
-const { connectMongoDB } = require('./mongodb/connectMongoDB');
-const app = express();
 const dns = require('dns');
 const mongoose = require('mongoose');
 const { url } = require('inspector');
 const { Schema, model } = mongoose;
-const urlShortenerScema = new Schema(
-  {
-    number: Number,
-    baseThirtySix: String,
-    url: String,
-    createdAt: Date,
-    submissions: Number
-  }
-)
-// Connect to MongoDB using Mongoose. Found in ./mongodb/connectMongoDB.js
-connectMongoDB();
-// Basic Configuration
-const port = process.env.PORT || 3000;
+const express = require('express');
+const cors = require('cors');
 
-app.use(cors());
-
-app.use('/public', express.static(`${process.cwd()}/public`));
-
-app.get('/', function(req, res) {
-  res.sendFile(process.cwd() + '/views/index.html');
+const clearURLDB = () => {
+    Url.deleteMany({number: {$gte: 0}}).then((data) => {
+    console.log(data);
+}).catch((err) => {
+    console.error(err);
 });
+};
 
-app.use(express.urlencoded({
-  limit: '10mb',
-  extended: true
-}));
+const urlShortRedirect = (req, res) => {
+    let urlString = req.params.shortUrlString;
+    Url.find({baseThirtySix: urlString})
+        .then((data) => {
+            if (data.length > 0) {
+                res.redirect(data[0].url);
+            }
+        }
+    );
+};
 
-// Create URl Model for URL Shortener Service
-const Url = model('Url',urlShortenerScema);
-
-app.use(express.urlencoded({
-    limit: '10mb',
-    extended: true
-  }));
-
-// URL Shortener code
-app.get('/api/shorturl/:shortUrlString', (req, res) => {
-let urlString = req.params.shortUrlString;
-Url.find({baseThirtySix: urlString}).then((data) => {
-    if (data.length > 0) {
-    res.redirect(data[0].url);
-    }
-});
-});
-
-app.post('/api/shorturl', function(req, res) {
+const urlPostHandle = (req, res) => {
     dns.lookup(new URL(req.body.url).hostname, {
         all: true
     }, (err, addresses) => {
@@ -67,9 +39,6 @@ app.post('/api/shorturl', function(req, res) {
                 url: urlString
             }).then((data) => {
                 if (data.length > 0) {
-                    Url.updateOne({
-                      url: urlString
-                    },{submissions: (data[0].submissions + 1)}).then((data) => {return;}).catch((err) => {console.error(err);});
                     res.json({
                         original_url: data[0].url,
                         short_url: data[0].baseThirtySix
@@ -79,7 +48,7 @@ app.post('/api/shorturl', function(req, res) {
                     Url.find({}).sort({
                         number: -1
                     }).limit(1).then(function(data) {
-                        numberInt = data.length ? parseInt(data[0].number) + 1 : 1;
+                        numberInt = parseInt(data[0].number) + 1;
                         let numberBaseThirtySix = numberInt.toString(36);
                         let createdAtDate = new Date();
                         let submissionsInt = 1;
@@ -109,8 +78,7 @@ app.post('/api/shorturl', function(req, res) {
             });
         }
     });
-});
+};
 
-app.listen(port, function() {
-  console.log(`Listening on port ${port}`);
-});
+module.exports.urlShortRedirect = urlShortRedirect;
+module.exports.urlPostHandle = urlPostHandle;
